9c61132e2bbcdbfd21911f17ec30528a410cb8ff
SAAD_KHAN <saad.khan@zortik.com>
2023-08-19 21:23:06 +0500

    tweet CRUD

--- src/Routes/tweetRouter.ts
+++ src/Routes/tweetRouter.ts
@@ -1,23 +1,49 @@
 import { Router } from "express";
-
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
-
+const prisma = new PrismaClient();
 // create tweet
-router.post("/", (req, res) => {
-    res.status(500).json({ error: "not Implemented" });
+router.post("/", async (req, res) => {
+    try {
+        const {content, image, userId } = req.body;
+        const result = await prisma.tweet.create({data: req.body})
+        console.log(req.body);
+        res.json(result);
+    }
+    catch (error) {
+        console.log(error)
+        res.status(400).json({error:"Something went Wrong . "});
+    }
+    
 })
 
 // list tweets
-router.get("/", (req, res) => {
-    res.status(500).json({ error: "not Implemented" });
+router.get("/", async (req, res) => {
+    try 
+    {
+    const allTweet = await prisma.tweet.findMany();
+    res.json(allTweet);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(400).json({error:"Something went Wrong . "});
+    }
 })
 
 
 // get one tweet
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     const { id } = req.params;
-    res.status(500).json({ error: `not Implemented ${id}` });
+    try {
+
+        const tweet = await prisma.tweet.findUnique({ where: { id: Number(id) } })
+        res.json(tweet);
+    }
+    catch (error) 
+    {
+        res.status(400).json({error:"Something went Wrong . "});
+    }
 })
 
 // update tweet
@@ -28,9 +54,15 @@ router.put("/:id", (req, res) => {
 
 
 // delete tweet
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
     const { id } = req.params;
-    res.status(500).json({ error: `not Implemented ${id}` });
+    try {    
+        const tweet = await prisma.tweet.delete({ where: { id: Number(id) } })
+        res.sendStatus(200);
+    }
+    catch (error) {
+        res.status(200).json({ error: `Cannot Deleted` });      
+    }
 })
 
 
